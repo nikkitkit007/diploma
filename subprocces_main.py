e7a8748210c20c060f98d92cfc84b6d848482848
nikita <nikkitkit0707@mail.ru>
2023-03-19 14:00:07 +0300

    checkpoint

--- subprocces_main.py
+++ subprocces_main.py
@@ -6,23 +6,21 @@ import yaml
 config_file = "config.yaml"
 CONFIG = yaml.safe_load(open(config_file))
 
-input_dir = '/home/nikita/Desktop/one-pixel-attack-master/cifar10/test'
+input_dir = '/home/nikita/Desktop/one-pixel-attack-master/'+CONFIG["input_dir"]
 output_dir = '/home/nikita/Desktop/one-pixel-attack-master/'+CONFIG["output_dir"]
 
 max_proc = CONFIG["max_proc"]
 
 
 if __name__ == '__main__':
-    ls = os.listdir('cifar10/test/')
+    ls = os.listdir(input_dir)
 
-    # if not os.path.exists(output_dir):
-    #     os.makedirs(output_dir)
     procs = []
     for i in range(max_proc):
         for filename in ls[i::max_proc]:
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, filename)
-            p = subprocess.Popen(['python', 'non_targeted.py', '--config', "config.yaml", "--input", input_path])
+            p = subprocess.Popen(['python3', 'non_targeted.py', '--config', "config.yaml", "--input", input_path])
             procs.append(p)
             while len(procs) == max_proc:
                 print([pr.pid for pr in procs])
