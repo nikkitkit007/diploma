d9c28a6bc59a60d04d2014096c32c4905b3d5de2
nikita <nikkitkit0707@mail.ru>
2023-03-19 13:42:47 +0300

    add to conf output file

--- subprocces_main.py
+++ subprocces_main.py
@@ -1,11 +1,16 @@
 import os
 import subprocess
 import time
+import yaml
+
+config_file = "config.yaml"
+CONFIG = yaml.safe_load(open(config_file))
 
 input_dir = '/home/nikita/Desktop/one-pixel-attack-master/cifar10/test'
-output_dir = '/home/nikita/Desktop/one-pixel-attack-master/output'
+output_dir = '/home/nikita/Desktop/one-pixel-attack-master/'+CONFIG["output_dir"]
 
-MAXPROC = 8
+max_proc = CONFIG["max_proc"]
+
 
 if __name__ == '__main__':
     ls = os.listdir('cifar10/test/')
@@ -13,13 +18,13 @@ if __name__ == '__main__':
     # if not os.path.exists(output_dir):
     #     os.makedirs(output_dir)
     procs = []
-    for i in range(MAXPROC):
-        for filename in ls[i::MAXPROC]:
+    for i in range(max_proc):
+        for filename in ls[i::max_proc]:
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, filename)
             p = subprocess.Popen(['python', 'non_targeted.py', '--config', "config.yaml", "--input", input_path])
             procs.append(p)
-            while len(procs) == MAXPROC:
+            while len(procs) == max_proc:
                 print([pr.pid for pr in procs])
                 for p in procs:
                     if p.poll() is not None:
